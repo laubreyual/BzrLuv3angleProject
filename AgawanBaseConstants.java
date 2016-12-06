a805ae445076f62f120f1d322fc1c74cd9f39bd2
laubsofficial <laubreyual@gmail.com>
2016-12-06 17:52:22 +0800

    Added class for declaring constants

---
+++ AgawanBaseConstants.java
@@ -0,0 +1,12 @@
+public interface AgawanBaseConstants {
+	public static final String APP_NAME="Agawan Base";
+	
+	//game states
+	public static final int GAME_START=0;
+	public static final int IN_PROGRESS=1;
+	public final int GAME_END=2;
+	public final int WAITING_FOR_PLAYERS=3;
+	
+	//game port
+	public static final int PORT=4444;
+}
